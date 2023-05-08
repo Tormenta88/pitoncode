121ab894aa492512fd42b6b55a9d035c6e969196
Tormenta88 <tormenta8282@gmail.com>
2023-05-08 17:08:54 +0200

    Ciertamente
    Ciertamente

--- Clase_Prog1/clase9.py
+++ Clase_Prog1/clase9.py
@@ -1,27 +1,90 @@
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-inputa, inputb = int(input("Dime un número\n")), int(input("Dime otro número\n"))
+def initialize_weights(input_dim, grid_dim):
+    return np.random.rand(grid_dim[0], grid_dim[1], input_dim)
 
-def suma():
-    print(inputa+inputb)
+def find_bmu(pattern, SOM):
+    distances = np.linalg.norm(SOM - pattern, axis=2)
+    return np.unravel_index(np.argmin(distances), distances.shape)
 
-def resta():
-    print(inputa-inputb)
+def update_weights(SOM, pattern, bmu, learning_rate, neighborhood_radius):
+    for i in range(SOM.shape[0]):
+        for j in range(SOM.shape[1]):
+            distance = np.linalg.norm(np.array([i, j]) - np.array(bmu))
+            if distance <= neighborhood_radius:
+                influence = np.exp(-distance ** 2 / (2 * (neighborhood_radius ** 2)))
+                SOM[i, j] += learning_rate * influence * (pattern - SOM[i, j])
 
-def multiplicacion():
-    print(inputa*inputb)
+def train_SOM(dataset, SOM, num_iterations, learning_rate, neighborhood_radius):
+    for _ in range(num_iterations):
+        for pattern in dataset:
+            bmu = find_bmu(pattern, SOM)
+            update_weights(SOM, pattern, bmu, learning_rate, neighborhood_radius)
 
-def sqrt():
-    print(math.sqrt(inputa))
+# Generate sample data
+X, y = np.random.rand(100, 3), np.random.randint(0, 2, 100)
 
+# Initialize SOM
+input_dim = X.shape[1]
+grid_dim = (10, 10)
+SOM = initialize_weights(input_dim, grid_dim)
 
-diccionario = {
-    1 : suma,
-    2 : resta,
-    3 : multiplicacion,
-    4 : sqrt
-}
+# Train SOM
+learning_rate = 0.5
+neighborhood_radius = 2.0
+num_iterations = 1000
+train_SOM(X, SOM, num_iterations, learning_rate, neighborhood_radius)
 
-operacion = int(input("Dime que quieres hacer \nsuma = 1\nresta = 2\nmultiplicación = 3\nRaiz cuadrada = 4\n"))
-diccionario[operacion]()
+# Define and initialize matrices to 0
+mapa_clasificacion = np.zeros(grid_dim + (input_dim,))
+mapa_activaciones = np.zeros(grid_dim)
+mapa_distancias = np.zeros(grid_dim)
 
+# Classify patterns and calculate activation and distances maps
+for pattern in X:
+    bmu = find_bmu(pattern, SOM)
+    print("Patrón:", pattern, "Coordenadas de la BMU:", bmu)
+    mapa_clasificacion[bmu] = pattern
+    mapa_activaciones[bmu] += 1
+    mapa_distancias[bmu] += np.linalg.norm(SOM[bmu] - pattern)
+
+mapa_distancias /= mapa_activaciones
+
+# Calculate number of classes
+num_classes = len(np.unique(y))
+print("Número de clases:", num_classes)
+
+# Draw classification, activation, and distances maps
+plt.imshow(mapa_clasificacion, interpolation='nearest', aspect='auto')
+plt.title("Mapa de Clasificación")
+plt.show()
+
+plt.imshow(mapa_activaciones, interpolation='nearest', aspect='auto')
+plt.title("Mapa de Activaciones")
+plt.show()
+
+plt.imshow(mapa_distancias, interpolation='nearest', aspect='auto')
+plt.title("Mapa de Distancias")
+plt.show()
+
+# Calculate Quantization Error and Topological Error
+U = np.zeros(grid_dim)
+for i in range(grid_dim[0]):
+    for j in range(grid_dim[1]):
+        neighbors = []
+        if i > 0:
+            neighbors.append(SOM[i - 1, j])
+        if i < grid_dim[0] - 1:
+            neighbors.append(SOM[i + 1, j])
+        if j > 0:
+            neighbors.append(SOM[i, j - 1])
+        if j < grid_dim[1] - 1:
+            neighbors.append(SOM[i, j + 1])
+        U[i, j] = np.mean([np.linalg.norm(SOM[i, j] - neighbor) for neighbor in neighbors])
+
+qe = np.mean(U)
+te = len(np.where(U > 1.0)[0]) / (grid_dim[0] * grid_dim[1])
+
+print("Error de cuantificación:", qe)
+print("Error Topológico:", te)
--- nico.py
+++ nico.py
@@ -1,6 +1,44 @@
-ciertamente = 1
-lista = ["hola", ciertamente, 1, [0,3]]
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy
+"""
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+pendiente, intercepción, r, p, std_err = stats.linregress(x, y)
+def myfunc(x):
+ return pendiente * x + intercepción
+mymodel = list(map(myfunc, x))
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()"""
 
-for x in lista:
-    if x in lista:
-        print(f"{x} Si esta")
+"""
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+myline = numpy.linspace(1, 22, 100)
+plt.scatter(x, y)
+plt.plot(myline, mymodel(myline))
+plt.show()"""
+
+
+
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+
+
+
+from sklearn.cluster import KMeans
+datos = list(zip(x, y))
+inercias = []
+for i in range(1,11):
+ kmeans = KMeans(n_clusters=i)
+ kmeans.fit(datos)
+ inercias.append(kmeans.inertia_)
+plt.plot(range(1,11), inercias, marker='o')
+plt.title('Método del codo')
+plt.xlabel('Número de conglomerados')
+plt.ylabel('Inercia')
+plt.show()

a81476f8e91d8f525bf7873da13229acf874eed7
Tormenta88 <tormenta8282@gmail.com>
2023-09-05 17:05:44 +0200

    pruebas

---
+++ dodecaedro.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import keras as kr
+import decorator
+
+
+
+
+pq = open("/Users/tormenta88/ciertamente.txt")
+pqr = pq.readlines()
+print(pqr)
+print(type(pqr))
+print(type(pqr[0]))
+print(pqr[0])
+
+
+pq.close()

41af92408c69466a61e802916283514ca0b5b056
Tormenta88 <tormenta8282@gmail.com>
2023-09-28 15:40:44 +0200

    HASHCOSAS

--- A2C1/Algoritmos/clase2TablasHash.py
+++ A2C1/Algoritmos/clase2TablasHash.py
@@ -1,24 +1,20 @@
 class HashTable:
     def __init__(self, capacidad = 100):
         self.capacidad = capacidad
-        self.tabla = [None] * capacidad
+        #self.tabla = [None] * capacidad
         self.cantidad = 0
+        self.tabla = [[] for _ in range(capacidad)]
 
     def __hash__(self, clave):
         return sum(ord(char) for char in clave % self.capacidad)
     
-    def insertar(self, clave, valor):
-        indice = self.__hash__(clave)
-
-        if not self.tabla(indice):
-            self.tabla[indice] = valor
-            self.cantidadcantidad+=1
-
-        else:
-            lista = list(self.tabla[indice])
-            lista.append(valor)
-            self.tabla[clave] = lista
-            self.cantidad +=1
+    def funcionHASH(self, clave):
+        clave_str = str(clave)
+        valor_hash = 0
+        for x in clave_str:
+            valor_hash = (valor_hash+ord(x))%self.capacidad
+        A = 0.54656753287
+        retorno = int(self.cantidad*((valor_hash*A)%1))
 
     def obtener(self, clave):
         return self.tabla[self.__hash__(clave)]
